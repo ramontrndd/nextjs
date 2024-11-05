10cfa09ba68c55f18ebfec0cc09c379fce7cd86d
ramontrndd <134494678+ramontrndd@users.noreply.github.com>
2024-11-04 22:51:49 -0300

    feat: add Tailwind CSS configuration and utility functions, implement global styles and card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@
 export default function Home() {
   return (
     <div>
-      <h1>Hello,World!</h1>
+     <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
       
     </div>
   );
