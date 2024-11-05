970b7ba8c0ce8e41b8edf454d4b5b4d77edf2949
ramontrndd <134494678+ramontrndd@users.noreply.github.com>
2024-11-05 20:10:29 -0300

    feat: implement MongoDB connection utility with error handling; remove legacy dbConnect.js file and update package.json and package-lock.json with dotenv and mongoose types

---
+++ lib/db.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    const mongoUri = process.env.MONGODB_URI;
+    if (!mongoUri) {
+      throw new Error('MONGO_URI is not defined');
+    }
+
+    const conn = await mongoose.connect(mongoUri, {});
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${(error as Error).message}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
